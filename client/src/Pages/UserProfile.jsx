4b385d3d5bb54e7f41d449eab60558ff111e28a3
Pratikpanchal25 <pratikpanchal2505@gmail.com>
2026-01-09 11:48:03 +0530

    new commit

--- client/src/Pages/UserProfile.jsx
+++ client/src/Pages/UserProfile.jsx
@@ -11,6 +11,7 @@ import { IconButton } from "@mui/material";
 import { CameraAlt as CameraAltIcon, Settings as SettingsIcon } from '@mui/icons-material';
 import { createFollow, getAllPostsByUser, getUserData, updateUserProfile } from '../AppWrite/Apibase';
 import { Login } from "../Store/AuthSlice.js";
+import BackButton from '../Components/BackButton.jsx';
 
 export default function UserProfile() {
     const [userData, setUser] = useState(null);
